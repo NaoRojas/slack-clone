b2d43592e8a2845a60d1c153bef0c7c407e58189
naoreds <naorojash@gmail.com>
2025-02-08 16:22:53 -0600

    Add sign-in options with Google and GitHub to the SignInCard component

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -7,6 +7,9 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
+import { FaGithub } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
 
 export const SignInCard = () => {
   return (
@@ -38,6 +41,35 @@ export const SignInCard = () => {
             Continue
           </Button>
         </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            type="button"
+            className="w-full"
+            size="lg"
+            variant="outline"
+            disabled={false}
+          >
+            <FcGoogle className="size-5" />
+            Continue with Google
+          </Button>
+          <Button
+            type="button"
+            className="w-full"
+            size="lg"
+            variant="outline"
+            disabled={false}
+          >
+            <FaGithub className="size-5" />
+            Continue with Github
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Don&apos;t have an account? &nbsp;
+          <span className="text-sky-700 hover:underline cursor-pointer">
+            Sign Up
+          </span>
+        </p>
       </CardContent>
     </Card>
   )
