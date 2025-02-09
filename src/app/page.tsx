35da2bca690485372d303f5b629e97b7df626e59
naoreds <naorojash@gmail.com>
2025-02-08 19:36:37 -0600

    Add sign-out button to home page for user authentication flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { useAuthActions } from '@convex-dev/auth/react'
+
 export default function Home() {
+  const { signOut } = useAuthActions()
   return (
     <div>
       <p>You are logged in! </p>
+      <Button onClick={() => signOut()}>Sign out</Button>
     </div>
   )
 }
