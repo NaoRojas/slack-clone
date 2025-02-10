06d7ce809427d8e928a1f3d4deef83b31cc72d2c
naoreds <naorojash@gmail.com>
2025-02-09 18:39:10 -0600

    Add workspace creation modal hook and integrate with home page logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { UserButton } from '@/features/auth/components/user-button'
+import { useCreateWorkspaceModal } from '@/features/store/use-create-workspace-modal'
 import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces'
 import { useEffect, useMemo } from 'react'
 
 export default function Home() {
+  const [open, setOpen] = useCreateWorkspaceModal()
   const { data, isLoading } = useGetWorkspaces()
   const workspaceId = useMemo(() => {
     if (data) {
@@ -15,10 +17,10 @@ export default function Home() {
     if (isLoading) return
     if (workspaceId) {
       console.log('Redirecting to workspace', workspaceId)
-    } else {
-      console.log('No workspace found')
+    } else if (!open) {
+      setOpen(true)
     }
-  }, [isLoading, workspaceId])
+  }, [isLoading, workspaceId, open, setOpen])
 
   return (
     <div>
